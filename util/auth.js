704ebf64fb4734aa6ef3b80c5c4f678b20feed6c
Shiva Sankar <shivasankar19@outlook.com>
2023-06-27 22:21:06 +0530

    created a const to shorten the URL during multiuse

--- util/auth.js
+++ util/auth.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const API_Key = "AIzaSyDchoJjyGh5qGt14TZvGK--yYj6429mU-Y";
+
 function createUser() {
   axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDchoJjyGh5qGt14TZvGK--yYj6429mU-Y"
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_Key
   );
 }
 
