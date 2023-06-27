bda95cb23ad1ad71bb210cd481416544e350e8fc
Shiva Sankar <shivasankar19@outlook.com>
2023-06-27 22:26:33 +0530

    convert the post fn to async await to get response

--- util/auth.js
+++ util/auth.js
@@ -2,9 +2,14 @@ import axios from "axios";
 
 const API_Key = "AIzaSyDchoJjyGh5qGt14TZvGK--yYj6429mU-Y";
 
-function createUser() {
-  axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_Key
+async function createUser(email1, password1) {
+  const response = await axios.post(
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_Key,
+    {
+      email: email1,
+      password: password1,
+      returnSecureToken: true,
+    }
   );
 }
 
