d6cfcaeaf805df20513c4527a63e686cf4ebb8ec
Shiva Sankar <shivasankar19@outlook.com>
2023-06-27 22:18:07 +0530

    "API KEY" changed to custom project api key

--- util/auth.js
+++ util/auth.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 function createUser() {
   axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]"
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDchoJjyGh5qGt14TZvGK--yYj6429mU-Y"
   );
 }
+
+//API Key in the URL should be replaced with your API key which is available in the firebase project settings
