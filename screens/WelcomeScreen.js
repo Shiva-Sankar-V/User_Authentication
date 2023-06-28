895f8e408d49879c099b4ef8c9e276dc42690150
Shiva Sankar <shivasankar19@outlook.com>
2023-06-28 23:55:51 +0530

    fetching data for protected database

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 import { StyleSheet, Text, View } from "react-native";
 
 function WelcomeScreen() {
+  const [fetchMsg, setFetchMsg] = useState("");
+  useEffect(() => {
+    axios
+      .get(
+        "https://expensesummary-9fbaf-default-rtdb.firebaseio.com/message.json"
+      )
+      .then((response) => {
+        // console.log(response.data);
+        setFetchMsg(response.data);
+      });
+  }, []);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchMsg}</Text>
     </View>
   );
 }
