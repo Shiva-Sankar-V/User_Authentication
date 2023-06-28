0e06f883d10a4e6fe1bf98612663c7df25987619
Shiva Sankar <shivasankar19@outlook.com>
2023-06-29 00:25:48 +0530

    auto logged using last used credentials

--- store/auth-context.js
+++ store/auth-context.js
@@ -1,4 +1,6 @@
-import { createContext, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext({
   token: "",
@@ -10,8 +12,22 @@ export const AuthContext = createContext({
 function AuthContextProvider({ children }) {
   const [authToken, setAuthToken] = useState();
 
+  useEffect(() => {
+    async function getToken() {
+      const fetchedToken = await AsyncStorage.getItem("token");
+
+      if (fetchedToken) {
+        setAuthToken(fetchedToken);
+      }
+    }
+    getToken();
+  }, []);
+  //setItem requires two args 1st-key and 2nd will be a string. since token is already a string
+  // we can use it as it is.
+
   function authenticate(token) {
     setAuthToken(token);
+    AsyncStorage.setItem("token", token);
   }
 
   function logout() {
